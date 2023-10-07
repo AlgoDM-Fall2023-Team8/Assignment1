f23c22dc8d1460c4ddc6fd804fdd2f5f65badd7e
sakshig10 <2017.sakshi.gujarathi@ves.ac.in>
2023-10-07 01:26:47 -0400

    Four queries completed

--- main.py
+++ main.py
@@ -32,7 +32,11 @@ connection = engine.connect()
 question_queries = [
     "Find customers and their detailed customer data who have returned items bought from the catalog more than 20 percent the average customer returns for customers in a given state in a given time period. Order output by customer data.",
     "Find customers who tend to spend more money (net-paid) on-line than in stores.",
-    "Retrieve the items with the highest number of returns where the number of returns was approximately equivalent across all store, catalog and web channels (within a tolerance of +/- 10%), within the week ending a given date"
+    "Retrieve the items with the highest number of returns where the number of returns was approximately equivalent across all store, catalog and web channels (within a tolerance of +/- 10%), within the week ending a given date", 
+    "List all customers living in a specified city, with an income between 2 values.",
+    "For all web return reason calculate the average sales, average refunded cash and average return fee by different combinations of customer and sales types (e.g., based on marital status, education status, state and sales profit).",
+    "Rollup the web sales for a given year by category and class, and rank the sales among peers within the parent, for each group compute sum of sales, location with the hierarchy and rank within the group.",
+    "Count how many customers have ordered on the same day items on the web and the catalog and on the same day have bought items in a store"
 ]
 
 ##initally storing the session variables
@@ -120,6 +124,77 @@ def show_parameters_3():
 
         return first_date,second_date,third_date
 
+
+
+
+def show_parameters_5():
+        with st.form(key='parameters_4'):
+            col1,col2=st.columns(2)
+
+            time_period_year=pd.read_sql_query("Select distinct(d_year) from date_dim order by d_year;",engine)
+            ca_state=pd.read_sql_query(" Select distinct(ca_state) from customer_address order by ca_state",engine)
+              
+            maratial_status=pd.read_sql_query("Select distinct(cd_marital_status) from customer_demographics order by cd_marital_status;",engine)
+            education_status=pd.read_sql_query("Select distinct(cd_education_status) from customer_demographics order by cd_education_status;",engine)
+
+            with col1:
+             ca_state_1=st.selectbox("Pick the first state",ca_state)
+             ca_state_2=st.selectbox("Pick the second state",ca_state)
+             ca_satte_3=st.selectbox("Pick the third state",ca_state)
+
+            
+            with col2:
+             maratial_status_select=st.selectbox("Pick the second state",maratial_status)
+             education_status_select=st.selectbox("Pick the education",education_status)
+             year_select=st.selectbox("Pick the year",time_period_year)
+        
+            submit_query_1=st.form_submit_button('Submit the paramters',on_click=param,args=(1,))
+        
+        return ca_state_1,ca_state_2,ca_satte_3,maratial_status_select,education_status_select,year_select
+
+def show_parameters_4():
+        with st.form(key='parameters_5'):
+            col1,col2=st.columns(2)
+
+            city=pd.read_sql_query("Select distinct(ca_city) from customer_address order by ca_city;",engine)
+           # income=pd.read_sql_query(" Select distinct(ib_lower_bound) from income_band order by ib_lower_bound",engine)
+
+            with col1:
+             city_select=st.selectbox("Pick the city",city)
+ 
+                        
+
+            
+            with col2:
+             income=st.text_input("Pick the income")
+        
+            submit_query_1=st.form_submit_button('Submit the paramters',on_click=param,args=(1,))
+        
+        return city_select,income
+            
+def show_parameters_6():
+        with st.form(key='6'):
+             get_dms=pd.read_sql_query("Select distinct(d_month_seq) from date_dim order by d_month_seq;",engine)
+             no_of_dms=st.selectbox("Pick DMS",get_dms)
+             
+             submit_query_1=st.form_submit_button('Submit the paramters',on_click=param,args=(1,))
+
+        
+
+        return no_of_dms     
+            
+def show_parameters_7():
+        with st.form(key='7'):
+             get_dms_7=pd.read_sql_query("Select distinct(d_month_seq) from date_dim order by d_month_seq;",engine)
+             no_of_dms_7=st.selectbox("Pick DMS",get_dms_7)
+             
+             submit_query_1=st.form_submit_button('Submit the paramters',on_click=param,args=(1,))
+
+        
+
+        return no_of_dms_7
+
+
 if st.session_state.stage==1:
 
     if(option_selected==question_queries[0]):
@@ -130,6 +205,20 @@ if st.session_state.stage==1:
 
     if(option_selected==question_queries[2]):
          first_date,second_date,third_date=show_parameters_3()
+
+    if(option_selected==question_queries[4]):
+        ca_state_1,ca_state_2,ca_satte_3,maratial_status_select,education_status_select,year_select=show_parameters_5()
+
+    if(option_selected==question_queries[3]):
+         city_select,income=show_parameters_4()
+
+    if(option_selected==question_queries[5]):
+         no_of_dms=show_parameters_6()
+
+
+    if(option_selected==question_queries[6]):
+         no_of_dms_7=show_parameters_7()
+
 
 
 if st.session_state.runquery==1:
@@ -146,7 +235,7 @@ if st.session_state.runquery==1:
 
                             connection.close()
                             engine.dispose()
-    
+
     if(option_selected==question_queries[1]):
                         try:
                             add_price=int(price)
@@ -179,7 +268,74 @@ if st.session_state.runquery==1:
 
                             connection.close()
                             engine.dispose()        
-        
+      
+    if(option_selected==question_queries[3]):
+                        try:
+                             income=int(income)
+                             add_income=income+50000
+                             df=pd.read_sql_query(run_query_4(city_select,income,add_income),engine)
+                             
+                             print(city_select,income)
+                             print(run_query_4(city_select,income,add_income))
+                             if df.empty:
+                                st.write("No results found")
+                             else:
+                                st.write(df)
+                             
+                        finally:
+
+                            connection.close()
+                            engine.dispose()        
+    if(option_selected==question_queries[4]):
+                        try:
+                             df=pd.read_sql_query(run_query_5(maratial_status_select,education_status_select,year_select,ca_state_1,ca_state_2,ca_satte_3,),engine)
+                             
+                             
+                             if df.empty:
+                                st.write("No results found")
+                             else:
+                                st.write(df)
+                             
+                        finally:
+
+                            connection.close()
+                            engine.dispose()
+                            
+    if(option_selected==question_queries[5]):
+                        try:
+                             no_of_dms=int(no_of_dms)
+                             add_dms=no_of_dms+11
+                             df=pd.read_sql_query(run_query_6(no_of_dms,add_dms),engine)
+                             
+                             print(add_dms,no_of_dms)
+                             print(run_query_6(no_of_dms,add_dms))
+                             if df.empty:
+                                st.write("No results found")
+                             else:
+                                st.write(df)
+                             
+                        finally:
+
+                            connection.close()
+                            engine.dispose()        
+
+    if(option_selected==question_queries[6]):
+                        try:
+                             no_of_dms_7=int(no_of_dms_7)
+                             add_dms_7=no_of_dms_7+11
+                             df=pd.read_sql_query(run_query_7(no_of_dms_7,  add_dms_7),engine)
+                             
+                             print(add_dms_7,no_of_dms_7)
+                             if df.empty:
+                                st.write("No results found")
+                             else:
+                                st.write(df)
+                             
+                        finally:
+
+                            connection.close()
+                            engine.dispose()
+
 
 
 
